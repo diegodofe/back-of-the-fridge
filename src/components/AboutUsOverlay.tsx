6475c38c1db192d583e459bff0aee0a748e42092
AlexFerence <alexference23@gmail.com>
2023-01-28 23:32:04 -0500

    About us and how it works

--- src/components/AboutUsOverlay.tsx
+++ src/components/AboutUsOverlay.tsx
@@ -1,6 +1,19 @@
 import React, {useState} from 'react';
 import {Button, Modal} from 'antd';
 
+
+const aboutUs = "Alex, Ari, Diego and Nika are a team participating " +
+  "in McHacks 10, a hackathon event.They came together to " +
+  "create a website that would help users find recipes based on the " +
+  "ingredients they have on hand.The idea for the website came about " +
+  "when Alex was feeling lazy and didn\'t want to go to the store, but still " +
+  "wanted to make a dish with tzatziki, couscous and onions.He tried searching " +
+  "online for a recipe, but couldn't find one that fit his specific ingredients. " +
+  "That's when he decided to enter the ingredients into a language model like ChatGPT " +
+  "and found a recipe that looked delicious.The team is now working on developing a " +
+  "website that will make this process easier for others.";
+
+
 interface AboutUsOverlayProps {
   open: boolean;
   setOpen: (open: boolean) => void;
@@ -8,12 +21,8 @@ interface AboutUsOverlayProps {
 
 const AboutUsOverlay = ({open, setOpen}: AboutUsOverlayProps) => {
 
-  const [loading, setLoading] = useState(false);
-
   const handleOk = () => {
-    setLoading(true);
     setTimeout(() => {
-      setLoading(false);
       setOpen(false);
     }, 3000);
   };
@@ -25,32 +34,16 @@ const AboutUsOverlay = ({open, setOpen}: AboutUsOverlayProps) => {
   return (
     <Modal
       open={open}
-      title="Title"
+      title="About Us"
       onOk={handleOk}
       onCancel={handleCancel}
       footer={[
         <Button key="back" onClick={handleCancel}>
-          Return
-        </Button>,
-        <Button key="submit" type="primary" loading={loading} onClick={handleOk}>
-          Submit
-        </Button>,
-        <Button
-          key="link"
-          href="https://google.com"
-          type="primary"
-          loading={loading}
-          onClick={handleOk}
-        >
-          Search on Google
-        </Button>,
+          Done
+        </Button>
       ]}
     >
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
-      <p>Some contents...</p>
+      <p>{aboutUs}</p>
     </Modal>
   );
 };
---
+++ src/components/HowItWorks.tsx
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+import {Button, Modal} from 'antd';
+
+const howItWorks = "Our team built the website \"Back of the Fridge\" using the React and Next.js frameworks for the frontend. These frameworks allowed us to create a user-friendly interface and provided a smooth navigation experience for our users. For the backend, we chose to use the Python programming language and the Flask web framework. This combination allowed us to build a robust and scalable backend that could handle a large number of requests. We also connected to the Cohere NLP API to process the input and output recipes. This API allowed us to quickly and easily extract the ingredients from the user's input and match them with the appropriate recipes from our database. The combination of React, Next.js, Python, Flask, and the Cohere NLP API allowed us to build a powerful and efficient recipe website that is able to provide users with the recipes they are looking for quickly and easily.";
+
+interface HowItWorksProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+
+const HowItWorks = ({open, setOpen}: HowItWorksProps) => {
+
+  const handleOk = () => {
+    setTimeout(() => {
+      setOpen(false);
+    }, 3000);
+  };
+
+  const handleCancel = () => {
+    setOpen(false);
+  };
+
+  return (
+    <Modal
+      open={open}
+      title="About Us"
+      onOk={handleOk}
+      onCancel={handleCancel}
+      footer={[
+        <Button key="back" onClick={handleCancel}>
+          Done
+        </Button>
+      ]}
+    >
+      <p>{howItWorks}</p>
+    </Modal>
+  );
+};
+
+export default HowItWorks;
