2b0f0b6d6fb4276c7593c5621075a9245c594ca7
Alex Ference <alexference23@gmail.com>
2023-01-28 23:45:28 -0500

    Alex about us how it works (#10)
    * About us overlay
    
    * About us and how it works
    
    * Chef gif

--- src/components/LoaderOverlay.tsx
+++ src/components/LoaderOverlay.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
 import React from "react";
 
 export const LoaderOverlay = () => {
   return (
     <div className="align-center absolute z-10 flex min-h-screen min-w-full justify-center bg-gray-500/40">
-      <p className="m-auto">loading...</p>
+      <div className="loader">
+        {/* <Image src="./chef.gif" alt="logo" width={100} height={100} /> */}
+        <img src="./chef.gif" alt="logo" width={400} height={400} />
+      </div>
     </div>
   );
 };
