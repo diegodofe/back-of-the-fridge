beed47330b29d021bc8362dfe3fdfc9e0645909e
AlexFerence <alexference23@gmail.com>
2023-01-29 10:57:28 -0500

    Update RecipeDetails.tsx

--- src/components/RecipeDetails.tsx
+++ src/components/RecipeDetails.tsx
@@ -1,8 +1,8 @@
-import { Image } from "antd";
+import {Image} from "antd";
 import React from "react";
-import type { Recipe } from "../types/recipe";
+import type {Recipe} from "../types/recipe";
 
-export const RecipeDetails = ({ recipe }: { recipe: Recipe | undefined }) => {
+export const RecipeDetails = ({recipe}: {recipe: Recipe | undefined;}) => {
   if (!recipe) return null;
   return (
     <div className="flex gap-8">
@@ -10,13 +10,12 @@ export const RecipeDetails = ({ recipe }: { recipe: Recipe | undefined }) => {
         <Image
           src={"https://picsum.photos/500"}
           alt={recipe.title + "photo"}
-          style={{ objectFit: "cover", width: "100%" }}
+          style={{objectFit: "cover", width: "100%"}}
           height="75%"
           width="100%"
           preview={false}
         />
       </div>
-
       <div className="flex flex-1 flex-col gap-4">
         <h3 className="text-2xl font-bold">{recipe.title}</h3>
         <div>
@@ -45,8 +44,22 @@ export const RecipeDetails = ({ recipe }: { recipe: Recipe | undefined }) => {
               </li>
             ))}
           </ul>
+          <div>
+            <div style={{height: '30px', color: 'white'}}>!</div>
+            <a
+              style={{
+                backgroundColor: "#bdc3c7",
+                color: "white", padding: "10px", borderRadius: "5px"
+              }}
+              rel="noreferrer"
+              target="_blank"
+              href="https://www.google.com/search?q=Food+donation+stores+near+me&oq=Food+donation+stores+near+me&aqs=chrome..69i57j33i22i29i30l9.10790j0j7&sourceid=chrome&ie=UTF-8">
+              Or Donate!
+            </a>
+          </div>
         </div>
       </div>
+
     </div>
   );
 };
